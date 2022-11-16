4c795048468bc3bc220d38363502f775b2ecb4e1
Kinpert <bryansantos.fdtobc@gmail.com>
2022-11-16 15:03:45 -0300

    1F-001 feat: To Do List Layout and basic functions

--- lib/main.dart
+++ lib/main.dart
@@ -12,46 +12,130 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.amber,
         ),
-        home: Container(
-            color: Colors.black,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(color: Colors.brown, width: 75, height: 75),
-                    Container(color: Colors.redAccent, width: 75, height: 75),
-                    Container(
-                        color: Colors.orangeAccent, width: 75, height: 75),
-                  ],
+        home: Scaffold(
+            appBar: AppBar(
+                title: const Text('To Do List!'),
+                backgroundColor: Colors.amber),
+            body: ListView(children: const [
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: Tasks('Playing an instrument'),
+              ),
+              Tasks('Learn Flutter and Dart'),
+              Tasks('Watch Youtube videos')
+            ]),
+            floatingActionButton: FloatingActionButton(onPressed: () {})));
+  }
+}
+
+class Tasks extends StatefulWidget {
+  final String taskName;
+  const Tasks(this.taskName, {super.key});
+
+  @override
+  State<Tasks> createState() => _TaskState();
+}
+
+class _TaskState extends State<Tasks> {
+  int level = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Colors.black,
+            ),
+            height: 140,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Container(
-                        color: Colors.yellowAccent, width: 75, height: 75),
-                    Container(color: Colors.greenAccent, width: 75, height: 75),
-                    Container(color: Colors.blueAccent, width: 75, height: 75),
+                      width: 72,
+                      height: 100,
+                      color: Colors.black26,
+                    ),
+                    Container(
+                      width: 200,
+                      child: Text(
+                        widget.taskName,
+                        style: const TextStyle(
+                            fontSize: 20, overflow: TextOverflow.ellipsis),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              level++;
+                            });
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: const [
+                              Icon(Icons.arrow_drop_up),
+                              Text(
+                                'UP',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                        color: Colors.lightBlueAccent, width: 75, height: 75),
-                    Container(
-                        color: Colors.purpleAccent, width: 75, height: 75),
-                    Container(color: Colors.white, width: 75, height: 75),
-                  ],
-                )
-              ],
-            )));
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 8,
+                    ),
+                    child: Container(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        color: Colors.orangeAccent,
+                        value: level / 10,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Level: $level',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
