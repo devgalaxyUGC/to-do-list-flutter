da572218068c4dcb330da5631bafddbde8ba9870
Kinpert <bryansantos.fdtobc@gmail.com>
2023-01-04 10:56:53 -0300

    1F-004: Task Controller and MobX implementation

--- lib/presentation/controllers/task_controller.dart
+++ lib/presentation/controllers/task_controller.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_my_app/domain/entities/task_entity.dart';
 import 'package:flutter_my_app/domain/usecases/get_task_usecase.dart';
+import 'package:mobx/mobx.dart';
 
-class TaskController {
-  TaskController(this._getTaskUsecase) {
+part 'task_controller.g.dart';
+
+class TaskController = _TaskController with _$TaskController;
+
+abstract class _TaskController with Store {
+  _TaskController(this._getTaskUsecase) {
     fetch();
   }
 
   final IGetTaskUsecase _getTaskUsecase;
-  final List<TaskEntity> list = [];
+
+  @observable
+  var list = ObservableList<TaskEntity>();
 
   TaskEntity? taskInCache;
 
+  @action
   fetch() async {
     final result = await _getTaskUsecase.call();
 
     result.fold(
-        (error) => debugPrint(error.toString()),
-        (success) => {
-              taskInCache?.name = success.name,
-              taskInCache?.difficulty = success.difficulty,
-              taskInCache?.imgSrc = success.imgSrc,
-              taskInCache?.isFinished = success.isFinished,
-              list.add(success)
-            });
+      (error) => debugPrint(error.toString()),
+      (success) => {
+        taskInCache?.name = success.name,
+        taskInCache?.difficulty = success.difficulty,
+        taskInCache?.imgSrc = success.imgSrc,
+        taskInCache?.isFinished = success.isFinished,
+        list.add(success),
+        for (int i = 0; i < list.length; i++) {print(list.elementAt(i).name)}
+      },
+    );
   }
 }
